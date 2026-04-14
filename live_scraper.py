62e7f15d30496cca7d14af0b5d12663cba537d99
Michael Forte <mforte@scarletmail.rutgers.edu>
2026-04-14 18:37:09 -0400

    Final Fix: Reverting mock date to live production date

--- live_scraper.py
+++ live_scraper.py
@@ -99,7 +99,7 @@ def get_team_roster_data(team_id):
 
 def get_todays_matchups():
     eastern = pytz.timezone('US/Eastern')
-    today = "2026-04-12"
+    today = datetime.now(eastern).strftime('%Y-%m-%d')
 
     url = f"https://statsapi.mlb.com/api/v1/schedule?sportId=1&date={today}&hydrate=probablePitcher"
 
